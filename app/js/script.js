bd44a7a6ccdc7a9e65fab6a6a69113535bc4d409
Saydur Rahman <saydur.rahman@gravitichain.com>
2018-06-28 14:27:25 +0100

    Added fix for gulp file and added Hero

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,19 @@
-// For Navigation Menu
+// For Hamburger Icon
 document.querySelector('.hamBurgerWrap').addEventListener('click', function () {
-
     var getBurgerId = document.querySelector('#nav-icon');
     getBurgerId.classList.toggle('open');
-
-    // var getNav = document.querySelector('nav');
-    // getNav.classList.toggle('nav');
-
 });
+
+// check if Header scrolling
+window.onscroll = function () { myFunction() };
+
+var header = document.querySelector('header');
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+}
